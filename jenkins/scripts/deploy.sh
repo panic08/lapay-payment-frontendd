3ebd4cb58b6d79ba87c3cd1461d90c9208b5dd78
panic08 <andrey1010102008@gmail.com>
2023-04-12 21:22:04 +0300

    v4.0

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -2,7 +2,7 @@
 echo 'Starting deployament...'
 
 echo 'Chmod is successful'
-sudo chmod a+r docker-compose.yml
+sudo chmod a+r ./docker-compose.yml
 echo 'Chmod is great'
 docker-compose up
 echo 'wow)'
