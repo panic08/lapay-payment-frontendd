098437ffa47e11a68f25b2db065d822bac9aef43
panic08 <andrey1010102008@gmail.com>
2023-04-12 16:50:18 +0300

    v4.0

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -3,6 +3,5 @@ echo 'Starting deployament...'
 sudo chmod 777 .
 sudo chmod +r docker-compose.yml
 echo 'Chmod is successful'
-sudo docker build -t iisus:1.0.0 .
-sudo docker-compose up -d
+sudo docker-compose up
 echo 'wow)'
